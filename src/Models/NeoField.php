e563c64b7aa32acc8db594bde0c808197e68cb67
Freek van Rijt <freek@digitalnatives.nl>
2017-04-04 18:31:19 +0200

    Proof of concept for Neo Field import, modded some export stuff

--- src/Models/NeoField.php
+++ src/Models/NeoField.php
@@ -3,10 +3,10 @@
 namespace NerdsAndCompany\Schematic\Models;
 
 use Craft\Craft;
-use Craft\BaseModel;
 use Craft\FieldModel;
 use Craft\FieldGroupModel;
-use Craft\MatrixBlockTypeModel;
+use Craft\Neo_BlockTypeModel;
+use Craft\Neo_GroupModel;
 
 /**
  * Schematic Matrix Field Model.
@@ -73,24 +73,28 @@ class NeoField extends Field
 
         $groups = $this->getNeoService()->getGroupsByFieldId($field->id);
         $blockTypes = $this->getNeoService()->getBlockTypesByFieldId($field->id);
-        $blockTypeFieldDefinitions = [];
 
+        /** @var \Craft\Neo_BlockTypeModel $blockType */
         foreach ($blockTypes as $blockType) {
             $blockTypeDefinitions[$blockType->handle] = [
               'name' => $blockType->name,
-              'childBlocks' => $blockType->childBlocks,
-              'topLevel' => $blockType->topLevel,
-              'sortOrder' => $blockType->sortOrder,
-              'group' => $this->getNeoService()->getGroupsByFieldId($field->id)[0]->name,
-              'fieldLayout' => $this->getFieldsService()->getFieldLayoutDefinition($blockType->getFieldLayout())
+              'group' => !empty($groups) ? $groups[0]->name : null,
+              'fieldLayout' => $this->getFieldsService()->getFieldLayoutDefinition($blockType->getFieldLayout()),
+              'settings' => [
+                'maxBlocks' => $blockType->maxBlocks,
+                'maxChildBlocks' => $blockType->maxChildBlocks,
+                'childBlocks' => $blockType->childBlocks,
+                'topLevel' => $blockType->topLevel,
+                'sortOrder' => $blockType->sortOrder,
+              ]
+
+
               /*'id' => AttributeType::Number,
               'dateCreated' => AttributeType::DateTime,
               'dateUpdated' => AttributeType::DateTime,
               'fieldId' => AttributeType::Number,
               'fieldLayoutId' => AttributeType::String,
-              'handle' => AttributeType::String,
-              'maxBlocks' => AttributeType::Number,
-              'maxChildBlocks' => AttributeType::Number,*/
+              'handle' => AttributeType::String */
             ];
         }
 
@@ -130,13 +134,32 @@ class NeoField extends Field
      */
     public function populate(array $fieldDefinition, FieldModel $field, $fieldHandle, FieldGroupModel $group = null, $force = false)
     {
+        // Save the neo field itself
+        // @OTOD: Check that all settings are saved.
         parent::populate($fieldDefinition, $field, $fieldHandle, $group);
 
-        /** @var MatrixSettingsModel $settingsModel */
+        /** @var \Craft\Neo_SettingsModel $settingsModel */
         $settingsModel = $field->getFieldType()->getSettings();
         $settingsModel->setAttributes($fieldDefinition['settings']);
+        $settingsModel->setGroups($this->getGroups($fieldDefinition, $field, $force));
         $settingsModel->setBlockTypes($this->getBlockTypes($fieldDefinition, $field, $force));
         $field->settings = $settingsModel;
+    }
+
+    protected function getGroups(array $fieldDefinition, FieldModel $field, $force = false)
+    {
+        // @TODO: When $force is true, look up groups by name and remove them if not in the export.
+        $groups = [];
+        if (isset($fieldDefinition['groups']) && is_array($fieldDefinition['groups'])) {
+            foreach ($fieldDefinition['groups'] as $name => $values) {
+                $group = new Neo_GroupModel();
+                $group->setAttributes($values);
+                $groups[] = $group;
+                // @TODO: Dedupe by name
+            }
+        }
+
+        return $groups;
     }
 
     /**
@@ -150,9 +173,10 @@ class NeoField extends Field
      */
     protected function getBlockTypes(array $fieldDefinition, FieldModel $field, $force = false)
     {
-        $blockTypes = $this->getMatrixService()->getBlockTypesByFieldId($field->id, 'handle');
+        $blockTypes = $this->getNeoService()->getBlockTypesByFieldId($field->id, 'handle');
 
-        //delete old blocktypes if they are missing from the definition.
+        // delete old blocktypes if they are missing from the definition.
+        // @TODO does this work?
         if ($force) {
             foreach ($blockTypes as $key => $value) {
                 if (!array_key_exists($key, $fieldDefinition['blockTypes'])) {
@@ -164,48 +188,22 @@ class NeoField extends Field
         foreach ($fieldDefinition['blockTypes'] as $blockTypeHandle => $blockTypeDef) {
             $blockType = array_key_exists($blockTypeHandle, $blockTypes)
               ? $blockTypes[$blockTypeHandle]
-              : new MatrixBlockTypeModel();
+              : new Neo_BlockTypeModel();
 
             $blockType->fieldId = $field->id;
             $blockType->name = $blockTypeDef['name'];
             $blockType->handle = $blockTypeHandle;
 
-            $this->populateBlockType($blockType, $blockTypeDef);
+            // Assign field layout
+            $layout = $this->getFieldsService()->getFieldLayout($blockTypeDef['fieldLayout']);
+            $blockType->setFieldLayout($layout);
+
+            // Add settings
+            $blockType->setAttributes($blockTypeDef['settings']);
 
             $blockTypes[$blockTypeHandle] = $blockType;
         }
 
         return $blockTypes;
-    }
-
-    /**
-     * Populate blocktype.
-     *
-     * @param BaseModel $blockType
-     * @param array     $blockTypeDef
-     */
-    protected function populateBlockType(BaseModel $blockType, array $blockTypeDef)
-    {
-        $fieldFactory = $this->getFieldFactory();
-
-        $blockTypeFields = [];
-        foreach ($blockType->getFields() as $blockTypeField) {
-            $blockTypeFields[$blockTypeField->handle] = $blockTypeField;
-        }
-
-        $newBlockTypeFields = [];
-
-        foreach ($blockTypeDef['fields'] as $blockTypeFieldHandle => $blockTypeFieldDef) {
-            $blockTypeField = array_key_exists($blockTypeFieldHandle, $blockTypeFields)
-              ? $blockTypeFields[$blockTypeFieldHandle]
-              : new FieldModel();
-
-            $schematicFieldModel = $fieldFactory->build($blockTypeFieldDef['type']);
-            $schematicFieldModel->populate($blockTypeFieldDef, $blockTypeField, $blockTypeFieldHandle);
-
-            $newBlockTypeFields[] = $blockTypeField;
-        }
-
-        $blockType->setFields($newBlockTypeFields);
     }
 }
